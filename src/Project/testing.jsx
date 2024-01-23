e784b7acbe8a4626b9ec033fc07074dd5242ca79
DG-LT-MT-DE314\DG-LT-MT-DE314 <karthi24102000@gmail.com>
2024-01-23 17:43:14 +0530

    first commit

---
+++ src/Project/testing.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+
+
+ const TestingMethod=()=>{
+
+    const [selectedOption, setSelectedOption] = useState('');
+  
+    const handleDropdownChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+
+
+
+    const runSmokeTest=(projectName)=>{
+        console.log("projectName------>", projectName);
+
+        fetchData(projectName);
+
+
+    }
+
+
+    const fetchData = async (val) => {
+        let finalVal  = val;
+        try {
+          const response = await fetch('http://13.233.138.49:3000/smoke-test', {
+            method: 'POST', // or 'GET', 'PUT', etc.
+            headers: {
+              'Content-Type': 'application/json',
+              // Add any additional headers if required
+            },
+            body: JSON.stringify({
+              projectPath: finalVal
+              // Add any data you want to send to the API
+            }),
+          });
+    
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+    
+          const result = await response.json();
+          console.log("result ------>", result)
+        } catch (error) {
+            console.log("result error ------>", error)
+        }
+      };
+
+
+    return(
+        <div>
+            <div> 
+                <div>
+                  <h3> <label > Select a project: </label></h3>
+                </div>
+      <select id="project" name="project" value={selectedOption} onChange={handleDropdownChange}>
+        <option value="Satbet-sports">Satbet_sports</option>
+        <option value="Satbet-WL">Satbet_WL</option>
+        <option value="BB-WL">BB_WL</option>
+      </select>
+      <p>Selected car: {selectedOption}</p></div>
+        <div> <button onClick={()=>{runSmokeTest(selectedOption)}}> Run the smoke test</button></div>
+        </div>
+    );
+}
+
+export default TestingMethod;
